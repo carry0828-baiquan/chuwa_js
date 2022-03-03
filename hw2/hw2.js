e11aaa331f74a8b7ccf3ed608b693d45b47316fb
baiquan <bshen0828@gmail.com>
2022-03-02 18:08:35 -0800

    using map and reduce

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -8,41 +8,33 @@ const itemsObject = [
 //1. Given the array, implement a function for generating a new array which doubles the
 // quantity and price in each object.
 
-function doubleQuantityAndPrice(Objects) {
-    // difference with for in
-    for (const object of Objects) {
-        object["quantity"] = object["quantity"] * 2;
-        object["price"] = object["price"] * 2;
-    }
-    console.log("The result for Q1 is " + JSON.stringify(Objects))
+function doubleQuantityAndPrice(obj) {
+    let container = {}
+    container["quantity"] = obj["quantity"] * 2
+    container["price"] = obj["price"] * 2
+    return container;
 }
-doubleQuantityAndPrice(itemsObject)
 
+let doubled = itemsObject.map(doubleQuantityAndPrice)
+
+console.log("Q1", doubled)
 // 2. Given the array, implement a function for generating a new array which contains item
 // quantity > 2 and price > 300 only
 
-const filterArray = function (Objects) {
-    let newArray = [];
-    for (const obj of Objects) {
-        if (obj["quantity"] > 2 && obj["price"] > 300) {
-            newArray.push(obj);
-        }
-    }
-    console.log("The result for Q2 is " + JSON.stringify(newArray));
+const filterArray = function (objects) {
+   return objects["quantity"] > 2 && objects["price"] > 300;
 };
 
-filterArray(itemsObject);
+let filtered = itemsObject.filter(filterArray)
 
+console.log("Q2", filtered)
 // 3. Given the array, implement a function to calculate the total value of the items.
-const calculateSum = (Objects) => {
-    let sum = 0;
-    for (const object of Objects) {
-        sum += object["quantity"] * object["price"]
-    }
-    console.log("The result for Q3 is " + sum)
+const calculateSum = (sum, objects) => {
+    return sum + objects["quantity"] * objects["price"]
 }
 
-calculateSum(itemsObject)
+let calculated = itemsObject.reduce(calculateSum, 0)
+console.log("Q3: ", calculated)
 
 //4. Given the string, implement a function to remove all the non-alphabet characters and
 // extra space in the string and convert the string to all lowercase
@@ -50,7 +42,8 @@ const string =
     ' Perhaps The Easiest-to-understand Case For Reduce Is To Return The Sum Of All The Elements In An Array ';
 
 const trimString = (string) => {
-  console.log("The result for Q4 is " + string.replace(/[^a-z0-9 -]/gi, '').toLowerCase().trim())
+  console.log("The result for Q4 is " + string.replace(/[^A-Za-z0-9]/g, ' ').toLowerCase().trim())
 }
+
 trimString(string);
 
